ebbfa3cc8cd6b07c35f0897d381c9e59b6f41fe3
Renal Eki Riyanto <ekysr99@gmail.com>
2025-03-09 15:08:57 +0700

    Get logged in profile user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
 const router = express.Router();
 
-// @route       GET /api/profile
-// @desc        Test route
-// @access      Public
-router.get('/', (req, res) => {
-    res.send('Profile route');
+// @route       GET /api/profile/me
+// @desc        Get logged in profile user
+// @access      Authenticated
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+
+        if(!profile){
+            return res.status(400).json({msg: 'User tidak memiliki profile'});
+        }
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
 })
 
 module.exports = router;
